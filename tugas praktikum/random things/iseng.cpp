dcb02c6cc85f5888f835c2150573858449747d20
Rafi A <Sleekfish8@gmail.com>
2025-12-06 08:38:02 +0700

    okz

--- tugas praktikum/random things/iseng.cpp
+++ tugas praktikum/random things/iseng.cpp
@@ -1,14 +1,14 @@
-#define jokowi cout
-
-
+// make ascending countdown from input number
 #include <iostream>
-
+#define ascending_countdown for (int i = 1; i <= n; i++) {std::cout << i << " ";}
+#define descending_countdown for (int i = n; i >= 1; i--) {std::cout << i << " ";}
 using namespace std;
 
-int main(){
-    jokowi << "Hello, World!" << endl;
-
-
+int main() {
+    int n;
+    cout << " enter a number: ";
+    cin >> n;
+    descending_countdown
 
     return 0;
 }
