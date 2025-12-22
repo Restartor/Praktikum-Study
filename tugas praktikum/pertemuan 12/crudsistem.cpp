4fa678910d9f49a61cf52f37ce5df5bf0611b56d
Rafi Andiansyah <rafiandiansyah01@gmail.com>
2025-12-22 15:46:06 +0700

    apaajalah

--- tugas praktikum/pertemuan 12/crudsistem.cpp
+++ tugas praktikum/pertemuan 12/crudsistem.cpp
@@ -1,13 +1,234 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
+/*STRUKTUR Mahasiswa
+    id      : integer
+    nim     : integer
+    nama    : string
+    jurusan : string
+AKHIR STRUKTUR
+*/
 struct Mahasiswa {
     int id;
-    string nim;
+    int nim;
     string nama;
-    string mataKuliah;
+    string jurusan;
 };
+/*PROSEDUR TambahData(data, total, autoID)
+    JIKA total >= 100 MAKA
+        TAMPILKAN "Kapasitas data penuh"
+        KEMBALI
+    AKHIR JIKA
 
+    data[total].id ← autoID
+    INPUT data[total].nim
+    INPUT data[total].nama
+    INPUT data[total].jurusan
+
+    total ← total + 1
+    autoID ← autoID + 1
+
+    TAMPILKAN "Data berhasil ditambahkan"
+AKHIR PROSEDUR
+*/
+void tambahData(Mahasiswa data[], int &total, int &autoID) {
+    if (total >= 100) {
+        cout << "Kapasitas data penuh\n";
+        return;
+    }
+    data[total].id = autoID;
+    cout << "NIM: ";
+    cin >> data[total].nim;
+    cout << "Nama: ";
+    cin >> data[total].nama;
+    cout << "Jurusan: ";
+    cin >> data[total].jurusan;
+
+    total++;
+    autoID++;
+    cout << "Data berhasil ditambahkan\n";
+}
+/*PROSEDUR TampilkanSemuaData(data, total)
+    JIKA total = 0 MAKA
+        TAMPILKAN "Data kosong"
+        KEMBALI
+    AKHIR JIKA
+
+    UNTUK i ← 0 SAMPAI total - 1 LAKUKAN
+        TAMPILKAN data[i].id, data[i].nim, data[i].nama, data[i].jurusan
+    AKHIR UNTUK
+AKHIR PROSEDUR
+*/
+void showAllData(Mahasiswa data[], int total) {
+    if (total == 0) {
+        cout << "Data kosong\n";
+        return;
+    }
+    for (int i = 0; i < total; i++) {
+        cout << data[i].id << " "
+             << data[i].nim << " "
+             << data[i].nama << " "
+             << data[i].jurusan << endl;
+    }
+}
+/*PROSEDUR TampilkanDataByID(data, total)
+    ditemukan ← SALAH
+    INPUT cariID
+
+    UNTUK i ← 0 SAMPAI total - 1 LAKUKAN
+        JIKA data[i].id = cariID MAKA
+            TAMPILKAN data[i].id, data[i].nim, data[i].nama, data[i].jurusan
+            ditemukan ← BENAR
+            HENTIKAN PERULANGAN
+        AKHIR JIKA
+    AKHIR UNTUK
+
+    JIKA ditemukan = SALAH MAKA
+        TAMPILKAN "Data tidak ditemukan"
+    AKHIR JIKA
+AKHIR PROSEDUR
+*/
+void showDataById(Mahasiswa data[], int total) {
+    int cariID;
+    bool ditemukan = false;
+    cout << "Masukkan ID: ";
+    cin >> cariID;
+    for (int i = 0; i < total; i++) {
+        if (data[i].id == cariID) {
+            cout << data[i].id << " "
+                 << data[i].nim << " "
+                 << data[i].nama << " "
+                 << data[i].jurusan << endl;
+            ditemukan = true;
+            break;
+        }
+    }
+    if (!ditemukan) {
+        cout << "Data tidak ditemukan\n";
+    }
+}
+/*PROSEDUR UbahDataByID(data, total)
+    ditemukan ← SALAH
+    INPUT cariID
+
+    UNTUK i ← 0 SAMPAI total - 1 LAKUKAN
+        JIKA data[i].id = cariID MAKA
+            INPUT data[i].nim
+            INPUT data[i].nama
+            INPUT data[i].jurusan
+            ditemukan ← BENAR
+            TAMPILKAN "Data berhasil diubah"
+            HENTIKAN PERULANGAN
+        AKHIR JIKA
+    AKHIR UNTUK
+
+    JIKA ditemukan = SALAH MAKA
+        TAMPILKAN "Data tidak ditemukan"
+    AKHIR JIKA
+AKHIR PROSEDUR
+*/
+void ubahDataById(Mahasiswa data[], int total) {
+    int cariID;
+    bool ditemukan = false;
+    cout << "Masukkan ID: ";
+    cin >> cariID;
+    for (int i = 0; i < total; i++) {
+        if (data[i].id == cariID) {
+            cout << "NIM baru: ";
+            cin >> data[i].nim;
+            cout << "Nama baru: ";
+            cin >> data[i].nama;
+            cout << "Jurusan baru: ";
+            cin >> data[i].jurusan;
+            ditemukan = true;
+            cout << "Data berhasil diubah\n";
+            break;
+        }
+    }
+    if (!ditemukan) {
+        cout << "Data tidak ditemukan\n";
+    }
+}
+/*PROSEDUR HapusDataByID(data, total)
+    ditemukan ← SALAH
+    INPUT cariID
+
+    UNTUK i ← 0 SAMPAI total - 1 LAKUKAN
+        JIKA data[i].id = cariID MAKA
+            UNTUK j ← i SAMPAI total - 2 LAKUKAN
+                data[j] ← data[j + 1]
+            AKHIR UNTUK
+
+            total ← total - 1
+            ditemukan ← BENAR
+            TAMPILKAN "Data berhasil dihapus"
+            HENTIKAN PERULANGAN
+        AKHIR JIKA
+    AKHIR UNTUK
+
+    JIKA ditemukan = SALAH MAKA
+        TAMPILKAN "Data tidak ditemukan"
+    AKHIR JIKA
+AKHIR PROSEDUR
+*/
+void hapusDataById(Mahasiswa data[], int &total) {
+    int cariID;
+    bool ditemukan = false;
+    cout << "Masukkan ID: ";
+    cin >> cariID;
+    for (int i = 0; i < total; i++) {
+        if (data[i].id == cariID) {
+            for (int j = i; j < total - 1; j++) {
+                data[j] = data[j + 1];
+            }
+            total--;
+            ditemukan = true;
+            cout << "Data berhasil dihapus\n";
+            break;
+        }
+    }
+    if (!ditemukan) {
+        cout << "Data tidak ditemukan\n";
+    }
+}
+/*
+MULAI
+    DEKLARASI data[100] SEBAGAI Mahasiswa
+    total ← 0
+    autoID ← 1
+
+    ULANGI
+        TAMPILKAN "MENU SISTEM PERKULIAHAN"
+        TAMPILKAN "1. Tambah Data"
+        TAMPILKAN "2. Tampilkan Semua Data"
+        TAMPILKAN "3. Tampilkan Data Berdasarkan ID"
+        TAMPILKAN "4. Ubah Data Berdasarkan ID"
+        TAMPILKAN "5. Hapus Data Berdasarkan ID"
+        TAMPILKAN "6. Keluar"
+
+        INPUT pilihan
+
+        PILIH pilihan
+            KASUS 1:
+                TambahData(data, total, autoID)
+            KASUS 2:
+                TampilkanSemuaData(data, total)
+            KASUS 3:
+                TampilkanDataByID(data, total)
+            KASUS 4:
+                UbahDataByID(data, total)
+            KASUS 5:
+                HapusDataByID(data, total)
+            KASUS 6:
+                SELESAI
+            LAINNYA:
+                TAMPILKAN "Pilihan tidak valid"
+        AKHIR PILIH
+
+    SELAMA pilihan ≠ 6
+SELESAI
+
+*/
 int main() {
     Mahasiswa data[100];
     int total = 0;
@@ -25,105 +246,27 @@ int main() {
         cout << "Pilih: ";
         cin >> pilihan;
 
-        if (pilihan == 1) {
-            data[total].id = autoID;
-            cout << "NIM: ";
-            cin >> data[total].nim;
-            cout << "Nama: ";
-            cin >> data[total].nama;
-            cout << "Mata Kuliah: ";
-            cin >> data[total].mataKuliah;
-
-            total++;
-            autoID++;
-
-            cout << "Data berhasil ditambahkan\n";
-        }
-
-        else if (pilihan == 2) {
-            if (total == 0) {
-                cout << "Data kosong\n";
-            } else {
-                for (int i = 0; i < total; i++) {
-                    cout << data[i].id << " "
-                         << data[i].nim << " "
-                         << data[i].nama << " "
-                         << data[i].mataKuliah << endl;
-                }
-            }
-        }
-
-        else if (pilihan == 3) {
-            int cariID;
-            bool ditemukan = false;
-
-            cout << "Masukkan ID: ";
-            cin >> cariID;
-
-            for (int i = 0; i < total; i++) {
-                if (data[i].id == cariID) {
-                    cout << data[i].id << " "
-                         << data[i].nim << " "
-                         << data[i].nama << " "
-                         << data[i].mataKuliah << endl;
-                    ditemukan = true;
-                    break;
-                }
-            }
-
-            if (!ditemukan) {
-                cout << "Data tidak ditemukan\n";
-            }
-        }
-
-        else if (pilihan == 4) {
-            int cariID;
-            bool ditemukan = false;
-
-            cout << "Masukkan ID: ";
-            cin >> cariID;
-
-            for (int i = 0; i < total; i++) {
-                if (data[i].id == cariID) {
-                    cout << "NIM baru: ";
-                    cin >> data[i].nim;
-                    cout << "Nama baru: ";
-                    cin >> data[i].nama;
-                    cout << "Mata Kuliah baru: ";
-                    cin >> data[i].mataKuliah;
-                    ditemukan = true;         
-                    cout << "Data berhasil diubah\n";
-                    break;
-                }
-            }
-
-            if (!ditemukan) {
-                cout << "Data tidak ditemukan\n";
-            }
-        }
-
-        else if (pilihan == 5) {
-            int cariID;
-            bool ditemukan = false;
-
-            cout << "Masukkan ID: ";
-            cin >> cariID;
-
-            for (int i = 0; i < total; i++) {
-                if (data[i].id == cariID) {
-                    for (int j = i; j < total - 1; j++) {
-                        data[j] = data[j + 1];
-                    }
-                    total--;
-                    ditemukan = true;
-                    cout << "Data berhasil dihapus\n";
-                    break;
-                }
-            }
-
-            if (!ditemukan) {
-                cout << "Data tidak ditemukan\n";
-            }
+        switch (pilihan) {
+            case 1:
+                tambahData(data, total, autoID);
+                break;
+            case 2:
+                showAllData(data, total);
+                break;
+            case 3:
+                showDataById(data, total);
+                break;
+            case 4:
+                ubahDataById(data, total);
+                break;
+            case 5:
+                hapusDataById(data, total);
+                break;
+            case 6:
+                break;
+            default:
+                cout << "Pilihan tidak valid\n";
+                break;
         }
 
     } while (pilihan != 6);
